266eca4799e7e1f43c22263f90c583db5765a191
devetude <devetude@naver.com>
2016-12-18 14:39:44 +0900

    add problem 1158

---
+++ src/boj_1158/Main.java
@@ -0,0 +1,45 @@
+package boj_1158;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 온라인 저지 1158번 (조세퍼스 문제) 문제풀이
+ *
+ * @see https://www.acmicpc.net/problem/1158
+ * @author devetude
+ */
+public class Main {
+	public static void main(String args[]) throws Exception {
+		// 버퍼를 통해 입력 값을 받음
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		br.close();
+
+		// 큐에 1부터 N까지 넣음
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		// 루프를 돌며 결과 값을 만듬
+		while (!queue.isEmpty()) {
+			for (int i = 1; i < M; i++) {
+				queue.add(queue.poll());
+			}
+
+			sb.append(queue.poll());
+		}
+
+		// 결과 값 출력
+		System.out.println(sb.toString());
+	}
+}
