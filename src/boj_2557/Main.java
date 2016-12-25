1b0759ad8e289e6c373c2a6d8e642143c710f11d
devetude <devetude@naver.com>
2016-12-26 02:35:06 +0900

    add problem 2557

---
+++ src/boj_2557/Main.java
@@ -0,0 +1,13 @@
+package boj_2557;
+
+/**
+ * 백준 온라인 저지 2557번 (Hello World) 문제풀이
+ *
+ * @see https://www.acmicpc.net/problem/2557
+ * @author devetude
+ */
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
