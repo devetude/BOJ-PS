a2be445263cae9cb0bcb9a2c5c6185c4875f0c18
devetude <devetude@naver.com>
2017-02-09 19:59:07 +0900

    update problem 2804

--- src/boj_2804/Main.java
+++ src/boj_2804/Main.java
@@ -5,9 +5,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 백준 온라인 저지 번 () 문제풀이
+ * 백준 온라인 저지 2804번 (크로스워드 만들기) 문제풀이
  *
- * @see https://www.acmicpc.net/problem/
+ * @see https://www.acmicpc.net/problem/2804
  * @author devetude
  */
 public class Main {
@@ -66,6 +66,7 @@ public class Main {
 			sb.append(NEW_LINE);
 		}
 
+		// 결과 값 한꺼번에 출력
 		System.out.println(sb.toString());
 	}
 }
