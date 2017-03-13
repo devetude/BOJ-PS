19998f0418a511b9af295bd200e397337525adc6
devetude <devetude@naver.com>
2017-03-13 22:08:39 +0900

    modify problem 1713

--- src/boj_1713/Main.java
+++ src/boj_1713/Main.java
@@ -1,10 +1,12 @@
 package boj_1713;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * 백준 온라인 저지 1713번 (후보 추천하기) 문제풀이
@@ -17,10 +19,13 @@ public class Main {
 	private static final String SPACE = " ";
 
 	public static void main(String args[]) throws Exception {
-		// 입력 값을 받음
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int cnt = sc.nextInt();
+		// 버퍼를 통해 입력 값을 받음
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int cnt = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		br.close();
 
 		// 큐 객체 변수 초기화
 		Queue<Integer> queue = new LinkedList<>();
@@ -29,7 +34,7 @@ public class Main {
 		int[] points = new int[cnt + 1];
 
 		for (int i = 0; i < cnt; i++) {
-			int idx = sc.nextInt();
+			int idx = Integer.parseInt(st.nextToken());
 			points[idx]++;
 
 			// 이미 해당 후보의 사진이 걸려있지 않은 경우에만
@@ -57,8 +62,6 @@ public class Main {
 				queue.offer(idx);
 			}
 		}
-
-		sc.close();
 
 		// 큐를 오름차순으로 정렬
 		Collections.sort((LinkedList<Integer>) queue);
