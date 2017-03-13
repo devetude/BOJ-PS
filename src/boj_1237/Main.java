a381ac74885e4977f23142fca83a2dbf060ee727
devetude <devetude@naver.com>
2017-03-13 15:46:24 +0900

    add problem 1237

---
+++ src/boj_1237/Main.java
@@ -0,0 +1,13 @@
+package boj_1237;
+
+/**
+ * 백준 온라인 저지 1237번 (정ㅋ벅ㅋ) 문제풀이
+ *
+ * @see https://www.acmicpc.net/problem/1237
+ * @author devetude
+ */
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("문제의 정답");
+	}
+}
