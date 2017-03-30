985abd3a59982e5256d81262305ad6ce3606e60b
devetude <devetude@naver.com>
2017-03-30 17:05:52 +0900

    add problem 1325

--- src/boj_1325/Main.java
+++ src/boj_1325/Main.java
@@ -14,14 +14,17 @@ import java.util.StringTokenizer;
  * @author devetude
  */
 public class Main {
+	// 공백 문자 상수
 	private static final char SPACE = ' ';
 
 	public static void main(String args[]) throws Exception {
+		// 버퍼를 통해 입력 값을 받음
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
+		// 인접리스트 객체 변수 초기화
 		ArrayList<Integer>[] lists = new ArrayList[N + 1];
 
 		for (int i = 1; i <= N; i++) {
@@ -38,47 +41,64 @@ public class Main {
 
 		br.close();
 
-		ArrayList<Integer> computers = new ArrayList<>();
-		int maxDepth = 0;
+		// 결과 컴퓨터 배열리스트 초기화
+		ArrayList<Integer> computers = new ArrayList<>(N + 1);
+
+		// 최대 해킹 가능한 컴퓨터 숫자 저장 변수 초기화
+		int maxCnt = 0;
 
 		for (int i = 1; i <= N; i++) {
-			int[] isVisited = new int[N + 1];
-			isVisited[i] = 1;
+			// 방문 여부 확인 배열 초기화
+			boolean[] isVisited = new boolean[N + 1];
+			isVisited[i] = true;
 
+			// 큐 객체 변수 초기화
 			Queue<Integer> queue = new LinkedList<>();
 			queue.offer(i);
 
+			// bfs 실행
 			while (!queue.isEmpty()) {
 				int current = queue.poll();
 
 				for (int next : lists[current]) {
-					if (isVisited[next] == 0) {
-						isVisited[next] = isVisited[current] + 1;
+					if (!isVisited[next]) {
+						isVisited[next] = true;
 
 						queue.offer(next);
 					}
 				}
 			}
 
-			for (int j = 1; j <= N; j++) {
-				if (maxDepth < isVisited[j]) {
-					maxDepth = isVisited[j];
-					computers.clear();
-					computers.add(i);
-				}
+			// 해킹 가능한 컴퓨터 숫자 저장 변수 초기화
+			int cnt = 0;
 
-				else if (maxDepth == isVisited[j] && !computers.contains(i)) {
-					computers.add(i);
+			for (int j = 1; j <= N; j++) {
+				if (isVisited[j]) {
+					cnt++;
 				}
+			}
+
+			// 저장되어있는 최대 해킹 가능한 컴퓨터 숫자보다 더 큰 값을 발견한 경우
+			if (maxCnt < cnt) {
+				maxCnt = cnt;
+				computers.clear();
+				computers.add(i);
+			}
+
+			// 저장되어있는 최대 해킹 가능한 컴퓨터 숫자와 같은 경우
+			else if (maxCnt == cnt) {
+				computers.add(i);
 			}
 		}
 
+		// 버퍼를 통해 결과 값을 만듬
 		StringBuilder sb = new StringBuilder();
 
 		for (int computer : computers) {
 			sb.append(computer).append(SPACE);
 		}
 
+		// 결과 값 한꺼번에 출력
 		System.out.println(sb.toString());
 	}
 }
