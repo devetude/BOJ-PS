0ee64156a1ee54115e137d1dfff0c7db25583556
devetude <devetude@naver.com>
2017-05-10 11:37:15 +0900

    modify problem 2667

--- src/boj_2667/Main.java
+++ src/boj_2667/Main.java
@@ -2,7 +2,10 @@ package boj_2667;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 백준 온라인 저지 2667번 (단지번호붙이기) 문제풀이
@@ -10,68 +13,88 @@ import java.util.Arrays;
  * @see https://www.acmicpc.net/problem/2667
  * @author devetude
  */
+
 public class Main {
 	// 개행 문자열 상수
 	private static final String NEW_LINE = "\n";
 
-	// 단지의 상태 상수
-	private static final int IS_HOUSE = 1;
-	private static final int IS_NOT_HOUSE = 0;
-
-	// 위, 아래, 오른쪽, 왼쪽 상수
-	private static int[] dx = { -1, 1, 0, 0 };
-	private static int[] dy = { 0, 0, -1, 1 };
-
-	// 지도의 크기, 지도 저장 배열
-	private static int N = 0;
-	private static int[][] map = null;
-
-	// 단지 인덱스, 단지별 포함되어있는 집의 갯수 저장 배열
-	private static int groupIdx = 0;
-	private static int[] houseCntInGroup = null;
+	// 방향 배열 상수
+	private static final int[][] DIRECTIONS = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+	private static final int ROW = 0;
+	private static final int COL = 1;
 
 	public static void main(String args[]) throws Exception {
 		// 버퍼를 통해 입력 값을 받음
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
 		// 지도 배열 초기화
-		map = new int[N][N];
+		int[][] map = new int[N][N];
 
-		// 단지별 포함되어있는 집의 갯수 저장 배열 초기화
-		houseCntInGroup = new int[N * N];
-
-		// 루프를 돌며 지도를 만듬
-		for (int i = 0; i < N; i++) {
+		for (int row = 0; row < N; row++) {
 			String line = br.readLine();
 
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Character.getNumericValue(line.charAt(j));
+			for (int col = 0; col < N; col++) {
+				map[row][col] = Character.getNumericValue(line.charAt(col));
 			}
 		}
 
 		br.close();
 
-		// 루프를 돌며 집인 경우 dfs를 시작
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j] == IS_HOUSE) {
-					// 단지 번호를 1 증가시킴 (새로운 단지가 형성된 것 임으로)
-					groupIdx++;
-					dfs(i, j);
+		// 방문 여부 저장 배열 초기화
+		boolean[][] isVisited = new boolean[N][N];
+
+		// 단지 갯수 저장 변수 초기화
+		int areaCnt = 0;
+
+		// 단지별 갯수 저장 배열리스트 초기화
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				// 집이 있는 곳이면서 bfs를 실행하지 않은 지점일 경우
+				if (map[row][col] == 1 && !isVisited[row][col]) {
+					areaCnt++;
+
+					int nodeCnt = 1;
+
+					isVisited[row][col] = true;
+
+					// 큐 객체 변수 초기화
+					Queue<Point> queue = new LinkedList<>();
+					queue.offer(new Point(row, col));
+
+					// bfs 실행
+					while (!queue.isEmpty()) {
+						Point current = queue.poll();
+
+						for (final int[] DIRECTION : DIRECTIONS) {
+							int nextRow = current.row + DIRECTION[ROW];
+							int nextCol = current.col + DIRECTION[COL];
+
+							if (0 <= nextRow && nextRow < N && 0 <= nextCol && nextCol < N) {
+								if (map[nextRow][nextCol] == 1 && !isVisited[nextRow][nextCol]) {
+									nodeCnt++;
+									isVisited[nextRow][nextCol] = true;
+									queue.offer(new Point(nextRow, nextCol));
+								}
+							}
+						}
+					}
+
+					list.add(nodeCnt);
 				}
 			}
 		}
 
-		// 단지별 포함되어있는 집의 갯수를 오름차순으로 정렬
-		Arrays.sort(houseCntInGroup, 1, groupIdx + 1);
-
 		// 버퍼를 통해 결과 값을 만듬
 		StringBuilder sb = new StringBuilder();
-		sb.append(groupIdx).append(NEW_LINE);
+		sb.append(areaCnt).append(NEW_LINE);
 
-		for (int i = 1; i <= groupIdx; i++) {
-			sb.append(houseCntInGroup[i]).append(NEW_LINE);
+		Collections.sort(list);
+
+		for (int nodeCnt : list) {
+			sb.append(nodeCnt).append(NEW_LINE);
 		}
 
 		// 결과 값 한꺼번에 출력
@@ -79,28 +102,23 @@ public class Main {
 	}
 
 	/**
-	 * dfs 메소드
+	 * 지점 이너 클래스
 	 * 
-	 * @param x
-	 * @param y
+	 * @author devetude
 	 */
-	private static void dfs(int x, int y) {
-		// 현재 지점을 집 -> 집이 아님 상태로 변경하여 다시 방문하지 않도록 설정
-		map[x][y] = IS_NOT_HOUSE;
+	private static class Point {
+		public int row;
+		public int col;
 
-		// 해당 단지의 포함되어있는 집의 갯수 1 증가
-		houseCntInGroup[groupIdx]++;
-
-		// 지도의 위, 아래, 오른쪽, 왼쪽을 찾아가며 집인 경우 다시 dfs를 시작
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-				if (map[nx][ny] == IS_HOUSE) {
-					dfs(nx, ny);
-				}
-			}
+		/**
+		 * 생성자
+		 * 
+		 * @param row
+		 * @param col
+		 */
+		public Point(int row, int col) {
+			this.row = row;
+			this.col = col;
 		}
 	}
 }
