9c78351643fe9896e712db4eda9f21c9cced921a
devetude <devetude@naver.com>
2023-12-11 12:23:47 +0900

    Add 10171 problem

---
+++ src/boj_10171/Main.java
@@ -0,0 +1,19 @@
+package boj_10171;
+
+/**
+ * 백준 온라인 저지 10171 (고양이) 문제풀이
+ *
+ * @author devetude
+ * @see https://www.acmicpc.net/problem/10171
+ */
+public class Main {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+
+        System.out.print(sb.toString());
+    }
+}
