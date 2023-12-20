ca9c792fb563d0342ab39e1b7116629206bcea1a
devetude <devetude@naver.com>
2023-12-20 15:05:15 +0900

    Solve the 15652 problem

---
+++ src/boj_15652/Kotlin.kt
@@ -0,0 +1,32 @@
+package boj_15652
+
+import java.util.StringTokenizer
+
+val sb = StringBuilder()
+
+fun main() {
+    val st = StringTokenizer(readln())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+
+    val arr = IntArray(m)
+    dfs(n, arr)
+
+    System.out.bufferedWriter().use {
+        it.write(sb.toString())
+        it.flush()
+    }
+}
+
+fun dfs(n: Int, arr: IntArray, startIdx: Int = 1, depth: Int = 0) {
+    if (depth == arr.size) {
+        arr.forEach { sb.append(it, " ") }
+        sb.appendLine()
+        return
+    }
+
+    for (idx in startIdx..n) {
+        arr[depth] = idx
+        dfs(n, arr, startIdx = idx, depth = depth + 1)
+    }
+}
