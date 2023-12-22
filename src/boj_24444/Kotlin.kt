31b0ca14d129aff1cca902341cb75865c9991684
devetude <devetude@naver.com>
2023-12-22 16:45:48 +0900

    Solve the 24444 problem

---
+++ src/boj_24444/Kotlin.kt
@@ -0,0 +1,41 @@
+package boj_24444
+
+import java.util.LinkedList
+import java.util.StringTokenizer
+import java.util.TreeSet
+
+fun main() {
+    var st = StringTokenizer(readln())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+    val r = st.nextToken().toInt()
+    val e = Array(size = n + 1) { TreeSet<Int>() }
+    repeat(m) {
+        st = StringTokenizer(readln())
+        val start = st.nextToken().toInt()
+        val end = st.nextToken().toInt()
+        e[start].add(end)
+        e[end].add(start)
+    }
+
+    val visitSequences = IntArray(size = n + 1)
+    var visitIdx = 1
+    val queue = LinkedList<Int>()
+    visitSequences[r] = visitIdx
+    queue.offer(r)
+    while (queue.isNotEmpty()) {
+        val start = queue.poll()
+        e[start].forEach { end ->
+            if (0 < visitSequences[end]) return@forEach
+
+            visitSequences[end] = ++visitIdx
+            queue.offer(end)
+        }
+    }
+    val result = buildString { for (i in 1..n) appendLine(visitSequences[i]) }
+
+    System.out.bufferedWriter().use {
+        it.write(result)
+        it.flush()
+    }
+}
