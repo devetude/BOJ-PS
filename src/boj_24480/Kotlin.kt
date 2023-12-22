7bd5187209cebd80d2c73b950aa05cf0da2ca202
devetude <devetude@naver.com>
2023-12-22 15:57:20 +0900

    Solve the 24480 problem

---
+++ src/boj_24480/Kotlin.kt
@@ -0,0 +1,40 @@
+package boj_24480
+
+import java.util.StringTokenizer
+import java.util.TreeSet
+
+var visitIdx = 0
+
+fun main() {
+    var st = StringTokenizer(readln())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+    val r = st.nextToken().toInt()
+    val e = MutableList(size = n + 1) { TreeSet<Int>(compareByDescending { it }) }
+    repeat(m) {
+        st = StringTokenizer(readln())
+        val start = st.nextToken().toInt()
+        val end = st.nextToken().toInt()
+        e[start].add(end)
+        e[end].add(start)
+    }
+
+    val visitSequences = IntArray(size = n + 1)
+    dfs(e, visitSequences, r)
+    val result = buildString { for (i in 1..n) appendLine(visitSequences[i]) }
+
+    System.out.bufferedWriter().use {
+        it.write(result)
+        it.flush()
+    }
+}
+
+fun dfs(e: MutableList<TreeSet<Int>>, visitSequences: IntArray, start: Int) {
+    visitSequences[start] = ++visitIdx
+
+    e[start].forEach { end ->
+        if (0 < visitSequences[end]) return@forEach
+
+        dfs(e, visitSequences, end)
+    }
+}
