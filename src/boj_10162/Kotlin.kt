dff1ce66792e137cfa8474ffe928dd4aa42e1bdd
devetude <devetude@naver.com>
2023-12-24 18:48:47 +0900

    Solve the 10162 problem

--- src/boj_10162/Kotlin.kt
+++ src/boj_10162/Kotlin.kt
@@ -1,22 +1,19 @@
 package boj_10162
 
-val TIME_SECS = arrayOf(300, 60, 10)
+val SECS = arrayOf(300, 60, 10)
 
 fun main() {
     var t = readln().toInt()
 
     val result = buildString {
-        TIME_SECS.forEach { timeSec ->
-            val timeSecCount = t / timeSec
-            t -= timeSec * timeSecCount
-            append(timeSecCount, " ")
-        }
-
-        if (0 < t) {
-            clear()
-            append(-1)
+        SECS.forEach {
+            val count = t / it
+            t -= it * count
+            append(count, " ")
         }
     }
+
+    if (0 < t) return println(-1)
 
     println(result)
 }
