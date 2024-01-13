131f9bd03bdb3683c857fa085abbfe35bb5e9c70
devetude <devetude@naver.com>
2024-01-14 00:39:22 +0900

    Solve the 14426 problem

---
+++ src/boj_14426/Kotlin.kt
@@ -0,0 +1,41 @@
+package boj_14426
+
+import java.util.StringTokenizer
+
+fun main() {
+    val st = StringTokenizer(readln())
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+
+    val trie = Trie()
+    repeat(n) { trie.insert(readln()) }
+
+    var count = 0
+    repeat(m) { if (trie.startsWith(readln())) ++count }
+
+    print(count)
+}
+
+class Trie {
+    private val root: Node = Node()
+
+    fun insert(word: String) {
+        var current = root
+        word.forEach { c ->
+            val child = current.children[c] ?: Node().also { current.children[c] = it }
+            current = child
+        }
+        current = current.copy(isTerminal = true)
+    }
+
+    fun startsWith(prefix: String): Boolean {
+        var current = root
+        prefix.forEach { c -> current = current.children[c] ?: return false }
+        return !current.isTerminal
+    }
+}
+
+data class Node(
+    val children: HashMap<Char, Node> = HashMap(),
+    val isTerminal: Boolean = false
+)
