9d8b746491279e7886b6fb82dd6bc30fe26cf36e
devetude <devetude@naver.com>
2024-01-14 13:48:30 +0900

    Solve the 16916 problem

--- src/boj_16916/Kotlin.kt
+++ src/boj_16916/Kotlin.kt
@@ -4,6 +4,33 @@ fun main() {
     val s = readln()
     val p = readln()
 
-    val result = if (-1 < s.indexOf(p)) 1 else 0
-    println(result)
+    val pi = pi(p)
+    val result = if (-1 < kmp(s, p, pi)) 1 else 0
+
+    print(result)
+}
+
+fun pi(pattern: String): IntArray {
+    val pi = IntArray(pattern.length)
+
+    var j = 0
+    for (i in 1 until pattern.length) {
+        while (0 < j && pattern[i] != pattern[j]) j = pi[j - 1]
+        if (pattern[i] == pattern[j]) pi[i] = ++j
+    }
+
+    return pi
+}
+
+fun kmp(str: String, pattern: String, pi: IntArray): Int {
+    var j = 0
+    for (i in str.indices) {
+        while (0 < j && str[i] != pattern[j]) j = pi[j - 1]
+        if (str[i] == pattern[j]) {
+            if (j == pattern.lastIndex) return pi[j]
+            ++j
+        }
+    }
+
+    return -1
 }
