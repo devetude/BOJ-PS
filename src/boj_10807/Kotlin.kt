36a1ef9c1f5badeb3773943cf81fe3a93409442a
devetude <devetude@naver.com>
2024-01-17 23:50:01 +0900

    Solve the 10807 problem

--- src/boj_10807/Kotlin.kt
+++ src/boj_10807/Kotlin.kt
@@ -5,8 +5,10 @@ import java.util.StringTokenizer
 fun main() {
     val n = readln().toInt()
     val st = StringTokenizer(readln())
-    val nums = ByteArray(n) { st.nextToken().toByte() }
-    val v = readln().toByte()
+    val v = readln()
 
-    print(nums.count { it == v })
+    var count = 0
+    repeat(n) { if (st.nextToken() == v) ++count }
+
+    print(count)
 }
