85f1d25448d2537b25c3410faa2e7ac65d0cfeba
devetude <devetude@naver.com>
2024-01-17 18:36:18 +0900

    Refactor the code

--- src/boj_2444/Kotlin.kt
+++ src/boj_2444/Kotlin.kt
@@ -3,21 +3,17 @@ package boj_2444
 fun main() {
     val n = readln().toInt()
 
-    val result = buildString {
-        for (rowIdx in 0 until n) appendStars(rowIdx, n)
-        for (rowIdx in n - 2 downTo 0) appendStars(rowIdx, n)
+    System.out.bufferedWriter().use { bw ->
+        repeat(n) { row ->
+            repeat(times = n - row - 1) { bw.write(" ") }
+            repeat(times = (row shl 1) + 1) { bw.write("*") }
+            bw.write("\n")
+        }
+        for (i in n - 1 downTo 1) {
+            repeat(times = n - i) { bw.write(" ") }
+            repeat(times = (i shl 1) - 1) { bw.write("*") }
+            bw.write("\n")
+        }
+        bw.flush()
     }
-
-    System.out.bufferedWriter().use {
-        it.write(result)
-        it.flush()
-    }
-}
-
-fun StringBuilder.appendStars(rowIdx: Int, n: Int) {
-    val starCount = rowIdx * 2 + 1
-    val indentCount = n - rowIdx - 1
-    for (indentIdx in 0 until indentCount) append(" ")
-    for (starIdx in 0 until starCount) append("*")
-    appendLine()
 }
