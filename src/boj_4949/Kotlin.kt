705d41756ff669a597ecd46a74ff8ac0cdd219e9
devetude <devetude@naver.com>
2024-01-21 16:57:08 +0900

    Refactor the code

--- src/boj_4949/Kotlin.kt
+++ src/boj_4949/Kotlin.kt
@@ -4,16 +4,12 @@ import java.util.Stack
 
 fun main() {
     val result = buildString {
-        while (true) {
-            val line = readln()
-            if (line == ".") break
-
+        var line: String
+        while (readln().also { line = it } != ".") {
             val stack = Stack<Char>()
             for (c in line) {
                 when (c) {
-                    '(', '[' -> {
-                        stack.push(c)
-                    }
+                    '(', '[' -> stack.push(c)
 
                     ')' -> {
                         if (stack.isEmpty() || stack.peek() != '(') {
@@ -31,9 +27,7 @@ fun main() {
                         stack.pop()
                     }
 
-                    else -> {
-                        Unit
-                    }
+                    else -> Unit
                 }
             }
             appendLine(if (stack.isEmpty()) "yes" else "no")
