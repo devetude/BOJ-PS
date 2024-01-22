f55e209896b855f1d205fb5ca2fbc123608a0095
devetude <devetude@naver.com>
2024-01-22 23:18:45 +0900

    Solve the 15688 problem

--- src/boj_15688/Kotlin.kt
+++ src/boj_15688/Kotlin.kt
@@ -1,12 +1,18 @@
 package boj_15688
 
-fun main() {
-    val n = readln().toInt()
-    val arr = IntArray(n) { readln().toInt() }
-    arr.sort()
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.Arrays
 
-    System.out.bufferedWriter().use {
-        it.write(arr.joinToString(separator = "\n"))
-        it.flush()
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val arr = IntArray(br.readLine().toInt()) { br.readLine().toInt() }
+    Arrays.sort(arr)
+
+    val sb = StringBuilder()
+    arr.forEach {
+        sb.append(it)
+        sb.append("\n")
     }
+    print(sb.toString())
 }
