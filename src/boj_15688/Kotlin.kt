36b239fc965a77c98b55b74205e5490ce2d041e6
devetude <devetude@naver.com>
2024-02-28 13:07:12 +0900

    Refactor the code

--- src/boj_15688/Kotlin.kt
+++ src/boj_15688/Kotlin.kt
@@ -1,18 +1,19 @@
 package boj_15688
 
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
-import java.util.Arrays
+import java.io.OutputStreamWriter
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val arr = IntArray(br.readLine().toInt()) { br.readLine().toInt() }
-    Arrays.sort(arr)
+    val counts = IntArray(size = 2_000_001)
+    repeat(br.readLine().toInt()) { ++counts[br.readLine().toInt() + 1_000_000] }
 
     val sb = StringBuilder()
-    arr.forEach {
-        sb.append(it)
-        sb.append("\n")
-    }
-    print(sb.toString())
+    counts.forEachIndexed { i, count -> repeat(count) { sb.appendLine(value = i - 1_000_000) } }
+
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    bw.write(sb.toString())
+    bw.flush()
 }
