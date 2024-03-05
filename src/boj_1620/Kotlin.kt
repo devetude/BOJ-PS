34dd1c9e5777aee9a1c9b70317ee050ff7a19ba7
devetude <devetude@naver.com>
2024-03-05 11:26:00 +0900

    Refactor the code

--- src/boj_1620/Kotlin.kt
+++ src/boj_1620/Kotlin.kt
@@ -1,6 +1,5 @@
 package boj_1620
 
-import java.util.HashMap
 import java.util.StringTokenizer
 
 fun main() {
@@ -8,23 +7,24 @@ fun main() {
     val n = st.nextToken().toInt()
     val m = st.nextToken().toInt()
 
-    val nameNumberMap = HashMap<String, Int>()
-    val numberNameMap = HashMap<Int, String>()
+    val nameOf = Array(size = n + 1) { "" }
+    val numOf = HashMap<String, Int>()
     repeat(n) {
+        val num = it + 1
         val name = readln()
-        nameNumberMap[name] = it + 1
-        numberNameMap[it + 1] = name
+        nameOf[num] = name
+        numOf[name] = num
     }
-    val results = buildString {
-        repeat(m) {
-            val input = readln()
-            val result = nameNumberMap[input] ?: numberNameMap[input.toInt()]
-            appendLine(result)
-        }
+
+    val sb = StringBuilder()
+    repeat(m) {
+        val input = readln()
+        val result = if (input[0] in '1'..'9') nameOf[input.toInt()] else numOf[input]
+        sb.appendLine(result)
     }
 
     System.out.bufferedWriter().use {
-        it.write(results)
+        it.write(sb.toString())
         it.flush()
     }
 }
