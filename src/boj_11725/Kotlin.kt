a7c816f48b646639863449968af9a24ae58f267e
devetude <devetude@naver.com>
2024-03-15 23:56:05 +0900

    Refactor the code

--- src/boj_11725/Kotlin.kt
+++ src/boj_11725/Kotlin.kt
@@ -3,35 +3,38 @@ package boj_11725
 import java.util.LinkedList
 import java.util.StringTokenizer
 
+private lateinit var adjacencyList: Array<LinkedList<Int>>
+private lateinit var parentOf: MutableList<Int>
+
 fun main() {
     val n = readln().toInt()
-    val graph = Array(size = n + 1) { LinkedList<Int>() }
+
+    adjacencyList = Array(size = n + 1) { LinkedList<Int>() }
     repeat(times = n - 1) {
         val st = StringTokenizer(readln())
-        val start = st.nextToken().toInt()
-        val end = st.nextToken().toInt()
-        graph[start].add(end)
-        graph[end].add(start)
+        val u = st.nextToken().toInt()
+        val v = st.nextToken().toInt()
+        adjacencyList[u].add(v)
+        adjacencyList[v].add(u)
     }
-    val queue = LinkedList<Int>()
-    queue.offer(1)
-    val parents = IntArray(size = n + 1)
-    while (queue.isNotEmpty()) {
-        val current = queue.poll()
 
-        graph[current].forEach { next ->
-            if (0 < parents[next]) return@forEach
+    parentOf = MutableList(size = n + 1) { 0 }.apply { this[1] = -1 }
+    dfs()
 
-            parents[next] = current
-            queue.offer(next)
-        }
-    }
-    val result = buildString {
-        for (child in 2..n) appendLine(parents[child])
-    }
+    val result = parentOf.subList(fromIndex = 2, toIndex = parentOf.size)
+        .joinToString(separator = "\n")
 
     System.out.bufferedWriter().use {
         it.write(result)
         it.flush()
+    }
+}
+
+private fun dfs(u: Int = 1) {
+    adjacencyList[u].forEach { v ->
+        if (parentOf[v] != 0) return@forEach
+
+        parentOf[v] = u
+        dfs(v)
     }
 }
